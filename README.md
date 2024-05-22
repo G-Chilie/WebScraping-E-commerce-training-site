35cff44d372ab7a6cb5822618342210556a84ad6
G-Chilie <okonkwolinda7@gmail.com>
2024-05-22 14:36:33 +0100

    Scraping of website with multiple pages

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Web Scraping with Beautiful Soup and Pandas - BeautifulSoup find_all() Methods
 
 Scraping a Table From a Website in Detailed using BeautifulSoup
+
+Web Scraping on Multiple WebSite
--- data.py
+++ data.py
@@ -1,51 +1,14 @@
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
 
-url = "https://ticker.finology.in/market/52-week-high"
-r = requests.get(url)
+for i in range(2,11):
+    url = "https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&p%5B%5D=facets.brand%255B%255D%3DOPPO&param=1001&ctx=eyJjYXJkQ29udGV4dCI6eyJhdHRyaWJ1dGVzIjp7InRpdGxlIjp7Im11bHRpVmFsdWVkQXR0cmlidXRlIjp7ImtleSI6InRpdGxlIiwiaW5mZXJlbmNlVHlwZSI6IlRJVExFIiwidmFsdWVzIjpbIk9wcG8gc21hcnRwaG9uZXMiXSwidmFsdWVUeXBlIjoiTVVMVElfVkFMVUVEIn19fX19&wid=21.productCard.PMU_V2_20"
+    r = requests.get(url)
+    print(r)
 
-soup = BeautifulSoup(r.text, "lxml")
+    soup = BeautifulSoup(r.text, "lxml")
 
-table = soup.find("table", class_ = "table table-sm table-hover screenertable")
-headers = table.find_all("th")
-
-titles = []
-for i in headers:
-    title = i.text
-    titles.append(title)
-
-    df = pd.DataFrame(columns=title)
-
-    rows = table.find_all("tr")
-
-for i in rows[1:]:
-    data = i.find_all("td")
-    ##print(data)
-    row = [tr.text for tr in data]
-    print(row)
-    l = len(df)
-    df.loc[l] = row
-
-print(df)    
-
-
-URL = "https://uk.indeed.com/jobs?q&l=Norwich%2C%20Norfolk&vjk=139a4549fe3cc48b"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-
-results = soup.find(id="ResultsContainer")
-
-job_elements = results.find_all("div", class_="resultContent")
-
-python_jobs = results.find_all("h2", string="Python")
-
-for job_element in job_elements:
-    title_element = job_element.find("h2", class_="jobTitle")
-    company_element = job_element.find("span", class_="companyName")
-    location_element = job_element.find("div", class_="companyLocation")
-    print(title_element)
-    print(company_element)
-    print(location_element)
-    print()
+    np = soup.find("a", class_ = "_1LKTO3").get("href")
+    cnp = "https://www.flipkart.com"+np
+    print(cnp)
+    
